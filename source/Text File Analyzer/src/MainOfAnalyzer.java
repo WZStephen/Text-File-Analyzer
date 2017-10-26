f91bde313cb9596baa160f1814d7b5e9881e8dd6
zhaoweichi123 <wzhao42@asu.edu>
2017-10-26 12:12:15 -0700

    12：12

---
+++ source/Text File Analyzer/src/MainOfAnalyzer.java
@@ -0,0 +1,31 @@
+
+
+import javax.swing.*;
+import java.util.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+public class MainOfAnalyzer extends JApplet
+{
+
+  private int APPLET_WIDTH = 700, APPLET_HEIGHT = 650;
+
+  private JTabbedPane tPane;
+  private HelpInfoPanel helpinfopane;
+  private AnaAndRepPanel anaandreepanel;
+  private OpenFilePanel openfilepanel;
+  private ArrayList filelist;;
+
+  public init()
+   {
+	  setTitle("Text File Analyzer");
+	  
+	  tPane = new JTabbedPane();
+	  
+
+
+   }
+  
+
+}
+
