e5eae9823c6e22cbaa4910bb42274dd28259d4ab
WINDOWS-C36HN9D\Stephen <wzhao42@asu.edu>
2017-10-26 17:34:20 -0700

    5:34   10/26

--- source/Text File Analyzer/src/MainOfAnalyzer.java
+++ source/Text File Analyzer/src/MainOfAnalyzer.java
@@ -3,39 +3,49 @@
 import javax.swing.*;
 import java.util.*;
 import java.awt.BorderLayout;
-import java.awt.Color;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
 
 public class MainOfAnalyzer extends JFrame
 {
 	private int APPLET_WIDTH = 700, APPLET_HEIGHT = 650;
 	
+	private JPanel wholePanel;
 	private JTabbedPane tabPane;
-   private OpenFilePanel openfilepanel;
-   private AnaAndRepPanel anaandreepanel;
-   private HelpInfoPanel helpinfopane;
+	private OpenFilePanel openfilepanel;
+	private AnaAndRepPanel anaandreepanel;
+	private HelpInfoPanel helpinfopane;
  
  
 
   public MainOfAnalyzer()
    {
-	  setTitle("Text File Analyzer");
+	  wholePanel = new JPanel();
 	  
+	  setTitle("Text File Analyzer"); //set the title in main page
+	  JLabel label1 = new JLabel();
+	  label1.setText("Text Analyzer");
+      JTabbedPane jtp = new JTabbedPane();
+      jtp.add(label1);
+	  getContentPane().add(jtp);
+
+      
+      
+      tabPane = new JTabbedPane();
 	  openfilepanel = new OpenFilePanel();
 	  anaandreepanel = new AnaAndRepPanel();
 	  helpinfopane = new HelpInfoPanel();
-      
-      tabPane = new JTabbedPane();
-	 
+    	 
       tabPane.addTab("Open File ", openfilepanel); // OpenFilePanel?
       tabPane.addTab("Analyzation Report", anaandreepanel);//AnaAndRepPanel?
 	  tabPane.addTab("Help", helpinfopane);//HelpInfoPanel?
 	  getContentPane().add(tabPane);
-	  setSize (APPLET_WIDTH, APPLET_HEIGHT); //set Applet size
-
+	  
+	  wholePanel.setLayout(new BoxLayout(wholePanel, BoxLayout.Y_AXIS));
+	  add(jtp);
+	  add(tabPane);
+	  
+	  setSize (APPLET_WIDTH, APPLET_HEIGHT); //set JFrame size
+	  
+	  
    }
   
   public static void main(String[] args) {
